b86142dfce474d998229fd3a3035f6a4c3bfabd8
zone12138 <1459547902@qq.com>
2024-07-21 17:42:41 +0800

    feature: add vue-router@4

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app-views">
+    <el-menu :default-active="activeIndex" mode="horizontal" router>
+      <el-sub-menu index="1">
+        <template #title> 首页 </template>
+        <el-menu-item index="/">Home</el-menu-item>
+      </el-sub-menu>
+      <el-menu-item index="/About">About</el-menu-item>
+    </el-menu>
+    <el-watermark class="content-views" content="Vue3 & Vite">
+      <router-view v-slot="{ Component }">
+        <KeepAlive>
+          <component :is="Component"></component>
+        </KeepAlive>
+      </router-view>
+    </el-watermark>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup lang="ts">
+import { ref } from "vue";
+const activeIndex = ref("1");
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.app-views {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.content-views {
+  flex: 1;
+  overflow: hidden;
 }
 </style>
